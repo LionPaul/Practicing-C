c0ed73ada934b82c6dc96c1cbd1a8376d053d203
Paulo Leão <ptalregister@gmail.com>
2025-05-21 18:03:22 -0300

    Update Ex4.c

--- section1/Ex4.c
+++ section1/Ex4.c
@@ -4,5 +4,18 @@
 
 int main()
 {
-    
+    int num1, num2;
+    printf("Digite o Primeiro Numero:");
+    scanf("%d", &num1);
+    printf("Digite o Segundo Numero:");
+    scanf("%d", &num2);
+
+    if (num1 > num2)
+    {
+        printf ("O numero maior: %d ",num1 );
+    }
+    else
+    {
+        printf ("O numero maior: %d ",num2);
+    }
 }
